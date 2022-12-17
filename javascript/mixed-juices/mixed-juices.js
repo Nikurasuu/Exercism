23e0a3ec27707f3cb2976bf816eef0770d5cf48e
Niklas Malkusch <niklas.malkusch@vier.ai>
2022-12-17 14:35:33 +0100

    Implemented all functions

--- javascript/mixed-juices/mixed-juices.js
+++ javascript/mixed-juices/mixed-juices.js
@@ -11,7 +11,21 @@
  * @returns {number} time in minutes
  */
 export function timeToMixJuice(name) {
-  throw new Error('Please implement the timeToMixJuice function');
+  //switch statement
+  switch (name) {
+    case "Pure Strawberry Joy":
+      return 0.5;
+    case "Energizer":
+      return 1.5;
+    case "Green Garden":
+      return 1.5;
+    case "Tropical Island":
+      return 3;
+    case "All or Nothing":
+      return 5;
+    default:
+      return 2.5;
+  }
 }
 
 /**
@@ -23,7 +37,28 @@ export function timeToMixJuice(name) {
  * @returns {number} number of limes cut
  */
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Please implement the limesToCut function');
+  let lemonsCut = 0;
+  let wedges = 0;
+  while (wedgesNeeded > wedges && limes[0] !== undefined){
+    switch (limes[0]) {
+      case "small":
+        wedges += 6;
+        lemonsCut++;
+        limes.shift();
+        break;
+      case "medium":
+        wedges += 8;
+        lemonsCut++;
+        limes.shift();
+        break;
+      case "large":
+        wedges += 10;
+        lemonsCut++;
+        limes.shift();
+        break;
+    }
+  }
+  return lemonsCut;
 }
 
 /**
@@ -34,5 +69,10 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Please implement the remainingOrders function');
+  let totalTime = 0;
+  while(totalTime < timeLeft){
+    totalTime += timeToMixJuice(orders[0]);
+    orders.shift();
+  }
+  return orders;
 }
